cd0227f305c303790a8e8f19c2d754ca7612d974
Michael Tobia <michaeltobia@gmail.com>
2018-07-19 20:18:06 -0400

    refactored bundling to new gallery setting

--- rollup.config.js
+++ rollup.config.js
@@ -1,37 +1,46 @@
 import buble from "rollup-plugin-buble";
-import glsl from "rollup-plugin-glsl";
+import glsl from "rollup-plugin-glslify";
+import json from "rollup-plugin-json";
 import ts from "rollup-plugin-typescript";
+import resolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
 
 import typescript from "typescript";
 
+import xp from "./src/xp.json";
+
 const plugins = {
+  resolve: resolve({
+    extensions: [".js", ".jsx", ".json"]
+  }),
+  cjs: commonjs({
+    include: "node_modules/**",
+    namedExports: {
+      react: [
+        "Component",
+        "PureComponent",
+        "Fragment",
+        "Children",
+        "createElement"
+      ],
+      "react-dom": ["render", "hydrate"]
+    }
+  }),
   buble: buble({
     transforms: {
-      classes: false,
+      // classes: false,
       forOf: false
     },
-    objectAssign: "Object.assign"
+    objectAssign: "Object.assign",
+    jsx: "React.createElement"
   }),
+  json: json(),
   glsl: glsl({
-    include: "src/**/*.glsl"
+    // include: "src/**/*.glsl"
   }),
   ts: ts({
     typescript
   })
-};
-
-const program = {
-  input: "src/program.js",
-  external: ["three"],
-  output: {
-    format: "iife",
-    file: "public/program.js",
-    interop: false,
-    globals: {
-      three: "THREE"
-    }
-  },
-  plugins: [plugins.glsl]
 };
 
 const gl = {
@@ -44,4 +53,23 @@ const gl = {
   plugins: [plugins.ts, plugins.buble]
 };
 
-export default [gl, program];
+const server = {
+  external: ["fs", "stream", "react", "react-dom/server", "express"],
+  input: "src/server/index.js",
+  output: { format: "cjs", file: "app.js" },
+  plugins: [plugins.resolve, plugins.json, plugins.buble]
+};
+
+const programs = [
+  ...xp.experiments.map(x => ({
+    input: x.src,
+    output: {
+      format: "iife",
+      file: x.file,
+      intro: "window.THREE = {};"
+    },
+    plugins: [plugins.cjs, plugins.resolve, plugins.glsl, plugins.ts]
+  }))
+];
+
+export default [...programs, server, gl];
